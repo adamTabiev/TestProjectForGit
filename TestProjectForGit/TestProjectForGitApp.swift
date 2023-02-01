70417c178e189ddce6a519df2ef8f27a804df1b5
adamTab <tabievadam@gmail.com>
2023-02-01 11:26:20 +0100

    Initial Commit

---
+++ TestProjectForGit/TestProjectForGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectForGitApp.swift
+//  TestProjectForGit
+//
+//  Created by Адам Табиев on 01.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectForGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
